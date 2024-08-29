4db5d7d6dac47ac8254656ef6ee70ce01caea623
thinhdh04 <tdh855513@gmail.com>
2024-08-29 11:11:56 +0700

    colors

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const App = () => {
+  return (
+   <div>
+    <h1 className="text-3xl font-bold underline">
+    Hello, World!
+   </h1>
+    <h1 className="bg-green-600 text-white border-4 border-red-600">
+    Hello, World!
+   </h1>
+   <h1 className="text-yellow-600">
+    Hello, World!
+   </h1>
+   <h1 className="text-932">
+    Hello, World!
+   </h1>
+   </div>
+  )
+}
+
+export default App
+
+// text-3xl font-bold underline
