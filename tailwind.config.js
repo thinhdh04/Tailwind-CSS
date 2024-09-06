cc84289cb8fac287f9bfe46febc4262735959f2e
thinhdh04 <tdh855513@gmail.com>
2024-09-06 17:41:38 +0700

    layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1100px',      
+    container: {
+      center: true,
     },
+    // screens: {
+    //   sm: '480px',
+    //   md: '768px',
+    //   lg: '976px',
+    //   xl: '1100px',      
+    // },
     // spacing: {
     //   '1': '8px',
     //   '2': '12px',
@@ -20,15 +23,15 @@ export default {
     //   '6': '48px',
     // },
     extend: {
-      fontSize: {
-        xs: '12px',
-        sm: '15px',
-        base: '18px'
-      },
-      colors: {
-        100: "#49e659",
-        932: "#434252"
-      },
+      // fontSize: {
+      //   xs: '12px',
+      //   sm: '15px',
+      //   base: '18px'
+      // },
+      // colors: {
+      //   100: "#49e659",
+      //   932: "#434252"
+      // },
     },
   },
   plugins: [],
